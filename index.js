fac09a93d035b982005d8d8f7cf31a882964a9e4
Ibrahim Salidu Kamara <ibrahimsallieuk@gmail.com>
2024-03-02 07:01:25 +0000

    More codes and script added and push to the remote repo 02-03-2024

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+let openHam = document.querySelector('#openHam');
+let closeHam = document.querySelector('#closeHam');
+let navigationItems = document.querySelector('#navigation-items');
+
+const hamburgerEvent = (navigation, close, open) => {
+    navigationItems.style.display = navigation;
+    closeHam.style.display = close;
+    openHam.style.display = open;
+};
+
+openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
+closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
+
 let profileMenu = document.getElementById("profileMenu");
 
 function toggleMenu(){
     profileMenu.classList.toggle("open-menu");
+}
+
+window.onload = function(e){
+    if (window.pageYoffset > 108) {
+        document.querySelector("nav").classList.add('is-scrolling');
+
+    } else {
+        document.querySelector("nav").classList.remove("is-scrolling");
+    }
+    const menu_btn = document.querySelector('.hamburger');
+    menu_btn.addEventListener('click', function(){
+        menu_btn.classList.toggle('is-active');
+    })
 }
